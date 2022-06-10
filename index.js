050cc5c3fb146e8fa6aa2a755d63dcf2ee21af3b
Jonathan Cernero <cernero.jon@gmail.com>
2022-06-10 17:15:57 -0400

    i lost count how many times this is taking me to fix

--- index.js
+++ index.js
@@ -7,22 +7,6 @@ const app = express();
 
 const APIURL = 'https://api.openweathermap.org';
 
-const whitelist = [
-  'https://jac-my-weatherclient.herokuapp.com',
-  'http://localhost:3000',
-];
-
-const corsOptionsDelegate = function (req, callback) {
-  let corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true };
-  } else {
-    corsOptions = { origin: false };
-  }
-  callback(null, corsOptions);
-};
-
-app.use(cors(corsOptionsDelegate));
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -30,19 +14,15 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.json('hi');
-});
+const issue2options = {
+  origin: true,
+  method: ['POST'],
+  credentials: true,
+  maxAge: 3600,
+};
 
-app.get('/getWeather', async (req, res) => {
-  const response = await axios.get(
-    `${APIURL}/data/2.5/onecall?lat=${req.query.latitude}&lon=${req.query.longitude}&appid=${process.env.REACT_APP_API_KEY}`
-  );
-  const data = response.data;
-  res.json(data);
-});
-
-app.post('/postWeather', async (req, res) => {
+app.options('/postWeather', cors(issue2options));
+app.post('/postWeather', cors(issue2options), async (req, res) => {
   const response = await axios.post(
     `${APIURL}/data/2.5/onecall?lat=${req.body.latitude}&lon=${req.body.longitude}&appid=${process.env.REACT_APP_API_KEY}`
   );
